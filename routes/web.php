32640c874e637344ef3410b00b3c43a11af7d1be
KONSTANTIN <konstantin.vrs@gmail.com>
2021-01-08 00:10:40 +0200

    added preliminary structure

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'MainController@admin');
+    Route::resource('/news', 'News\Admin\NewsController');
+    Route::resource('/guest-book', 'GuestBook\Admin\GuestBookController');
 });
+
+Route::get('/', 'MainController@index')->name('news');
