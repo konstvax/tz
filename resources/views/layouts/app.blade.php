9b58b4bd97f67db9f88c0bd3efd117b3df954ea4
KONSTANTIN <konstantin.vrs@gmail.com>
2021-01-08 00:40:20 +0200

    added bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,14 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Bootstrap CSS -->
-{{--        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"--}}
-{{--              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"--}}
-{{--              crossorigin="anonymous">--}}
-
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
         <title>Hello, world!</title>
     </head>
     <body>
         <div class="container">
             <h1>test</h1>
         </div>
-{{--        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"--}}
-{{--                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"--}}
-{{--                crossorigin="anonymous"></script>--}}
+        <script src="js/bootstrap.js"></script>
     </body>
 </html>
